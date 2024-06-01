3d2b6b4ee603aa982d787c98d8e3f68bd5a12267
3Doge <161205486+3Doge@users.noreply.github.com>
2024-06-01 16:22:40 +0100

    Create Benchmark.py

---
+++ Benchmark.py
@@ -0,0 +1,30 @@
+import time
+from datetime import datetime
+
+
+testings = 0
+
+FirstTime = currentDateAndTime = datetime.now()
+
+while True:
+    time.sleep(0.5)
+    LastTime = currentDateAndTime = datetime.now()
+    print("test test test test")
+    print("The current date and time is", currentDateAndTime)
+    testings += 1
+    print(testings)
+    if testings == 100:
+        break
+
+
+text1 = LastTime - FirstTime
+
+
+besttime = str(text1)[5:]
+
+
+print(besttime, "seconds")
+
+print("^ time in 100 mini tests")
+
+k=input("press enter to exit")
